a0085b9a454ca25eea3b77a4d5fdbeb5e4d7471d
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-06-09 14:42:20 +0200

    commit router
    commit router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Router from './router';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Router from "./router";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router />
--- src/router.js
+++ src/router.js
@@ -1,11 +1,24 @@
+import {Route,Routes,BrowserRouter} from 'react-router-dom'
 
+// FIRST BrowserRouter => Routes => Route
 
-
+import Home from './components/home'
+import PostComponent from './components/posts'
+import Contact from './components/contact'
 
 const Router = () => {
-  return(
-     <h1>hello</h1>
-  )
-}
+    return (
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+             <Route path='/contact' element={<Contact/>} />
+              <Route path='article/:id' element={<PostComponent/>} />
+          </Routes>
+        </BrowserRouter>
+      
+    
+
+    )
+};
 
 export default Router;
