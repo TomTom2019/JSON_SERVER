fb1852294fa43deb5c5aa860e67342ef3ff12e56
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-06-15 14:06:18 +0200

    final update
    final update

--- src/layouts/mainLayout.js
+++ src/layouts/mainLayout.js
@@ -1,13 +1,15 @@
 // layout will wrapp application
 // centralize place to add what you want apply to all route
 import {Container} from 'react-bootstrap'
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const MainLayout = (props)=>{
 	return(
       <Container className='mt-5 mb-5'>
 
       	{props.children}
+      	<ToastContainer/>
       </Container>
 
 
