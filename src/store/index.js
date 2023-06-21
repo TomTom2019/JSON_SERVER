ff730fd3aa71893996e03a3c97aa2e8a7daac354
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-06-21 14:23:26 +0200

    more post done

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+// postsSlice =>PostsReducer ..cant change name
+import {configureStore} from '@reduxjs/toolkit'
+import PostsReducer from './reducers/posts'
+import UsersReducer from  './reducers/users'
+
+
+export const store = configureStore({
+	reducer:{
+       posts:PostsReducer,
+       users:UsersReducer
+	}
+})
