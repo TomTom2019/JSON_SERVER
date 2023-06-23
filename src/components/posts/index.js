1aca9973763ff56c426fec2a03cf50ca6224de87
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-06-23 15:23:19 +0200

    postById + fix image

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -1,8 +1,54 @@
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux'
+import Moment from 'react-moment';
+import { fetchPostById } from '../../store/utils/thunks';
+
+
+
 const PostComponent = ()=>{
+  const posts = useSelector((state)=>state.posts)
+  const dispatch = useDispatch()
+  let params = useParams()
+
+/*params = id*/
+   useEffect(()=>{
+      dispatch(fetchPostById(params.id))
+   },[])
+
+/*postById come from reducer
+posts from thunks
+*/
+
     return(
-       <>
-       PostComponent
-       </>
+     <>
+            { posts.postById ?
+            <div className="article_container">
+                <h1>{posts.postById.title}</h1>
+                <div
+                    style={{
+                        background:`url(${posts.postById.imagexl})`
+                    }}
+                    className="image"
+                ></div>
+                <div className="author">
+                    Created by: <span>{posts.postById.author} - </span>
+                    <Moment format="DD MMMM">
+                        {posts.postById.createdAt} 
+                    </Moment>
+                </div>  
+                <div className="mt-3 content">
+                    <div dangerouslySetInnerHTML={{
+                        __html: posts.postById.content
+                    }}></div>
+                </div>
+            </div>
+            :null}
+
+         
+        
+
+        </>
 
         )
 }
