883fbd4771cc308715de5c94f020ea7cef04dfa7
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-06-26 15:10:39 +0200

    fix update

--- src/components/utils/newsletter.js
+++ src/components/utils/newsletter.js
@@ -1,17 +1,21 @@
 import {Form,Button} from 'react-bootstrap'
 import {useRef,useEffect} from 'react'
+import { useDispatch } from 'react-redux'
 
+import { addToNewsletter } from '../../store/utils/thunks'
 // e.preventDefault not reload page
 // preventDefault post data
 const Newsletter = ()=>{
     const textInput = useRef()
+      const dispatch = useDispatch();
+
 
    const handleSubmit = (e)=>{
    	e.preventDefault()
    	const value = textInput.current.value
    	console.log(value)
     /* create thunks =>create slice*/
-
+    dispatch(addToNewsletter({email:value}))
    }
 
 
