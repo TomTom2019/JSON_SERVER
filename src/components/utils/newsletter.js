1caf6e5d01af803bcaaf8f8303ab3a78cb1ae82a
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-06-27 14:18:15 +0200

    showToast

--- src/components/utils/newsletter.js
+++ src/components/utils/newsletter.js
@@ -1,21 +1,40 @@
 import {Form,Button} from 'react-bootstrap'
 import {useRef,useEffect} from 'react'
 import { useDispatch } from 'react-redux'
+import {showToast} from './tools'
 
 import { addToNewsletter } from '../../store/utils/thunks'
 // e.preventDefault not reload page
 // preventDefault post data
 const Newsletter = ()=>{
     const textInput = useRef()
-      const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
 
    const handleSubmit = (e)=>{
    	e.preventDefault()
    	const value = textInput.current.value
    	console.log(value)
-    /* create thunks =>create slice*/
+    /* create thunks =>create slice
+      unwrap work with thunks
+    */
     dispatch(addToNewsletter({email:value}))
+    .unwrap()
+    .then((response)=>{
+      if(response.newsletter === 'added'){
+        /* console.log('OK')*/
+          showToast('SUCCESS','Thanks to join us')
+         textInput.current.value = '';
+      }
+         if(response.newsletter === 'failed'){
+         /*console.log('ERROR')*/
+          showToast('ERROR','you are already on the list')
+         textInput.current.value = '';
+
+         }
+    })
+
+
    }
 
 
