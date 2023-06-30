2ee7d56749b3845486b935e48049ad34c02b2a11
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-06-30 14:31:36 +0200

    update

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,7 +1,45 @@
+// useFormik is hook
+import {useFormik} from 'formik'
+import * as Yup from 'yup'
+import {Alert} from 'react-bootstrap'
+import {useDispatch} from 'react-redux'
+
+
 const Contact = ()=>{
+  const dispatch = useDispatch()
+  const formik = useFormik({
+    initialValues:{email:'',firstname:'',lastname:'',message:''},
+    validationSchema: Yup.object({
+      email: Yup.string()
+      .required('email is required')
+      .email('your email is invalid'),
+      firstname:Yup.string()
+      .required('your name is required'),
+      lastname:Yup.string()
+      .required('your lastname is required'),
+      message:Yup.string()
+      .required('message is required')
+      .max(500,'your message is too long')
+      
+    }),
+    onSubmit:(values,{restForm})=>{
+      /// dispatch
+      console.log(values)
+    }
+
+  })
+
+
+
     return(
        <>
-       Contact
+       <h1>Contact us</h1>
+       <form className='mt-3' onSubmit={formik.handleSubmit}>
+          <div>
+             
+          </div>
+       </form>
+
        </>
 
         )
